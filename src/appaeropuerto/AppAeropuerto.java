da197ac8ecfbf3a26b291e7686a6fb829b192195
AlomiaTobar <AlomiaTobar@DESKTOP-QQN31HP>
2022-05-14 22:55:03 -0500

    hola

--- src/appaeropuerto/AppAeropuerto.java
+++ src/appaeropuerto/AppAeropuerto.java
@@ -14,9 +14,6 @@ public class AppAeropuerto {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.print("Hola mundo");
-    }
+    
     
 }
