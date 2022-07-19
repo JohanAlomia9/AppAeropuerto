3e2668c622f88c5e75be5edda5ceeaf5482202c0
sebas <sebas@DESKTOP-8L4KEKG>
2022-07-19 16:54:42 -0500

    hice conexiones

--- src/fmrFormularios/frmMenuPrincipal.java
+++ src/fmrFormularios/frmMenuPrincipal.java
@@ -45,21 +45,41 @@ public class frmMenuPrincipal extends javax.swing.JFrame {
         jButton1.setBackground(new java.awt.Color(204, 255, 204));
         jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ImagenesMenuPrincipal/avion (1).png"))); // NOI18N
         jButton1.setText("VUELOS DISPONIBLES");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
         getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 110, 420, -1));
 
         jButton2.setBackground(new java.awt.Color(255, 204, 204));
         jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ImagenesMenuPrincipal/tarjeta-de-embarque.png"))); // NOI18N
         jButton2.setText("COMPRAR BOLETOS");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
         getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 210, 420, -1));
 
         jButton3.setBackground(new java.awt.Color(204, 204, 255));
         jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ImagenesMenuPrincipal/billete.png"))); // NOI18N
         jButton3.setText("RESERVAR VUELOS");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
         getContentPane().add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 300, 420, -1));
 
         jButton4.setBackground(new java.awt.Color(204, 255, 204));
         jButton4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ImagenesMenuPrincipal/comunicar.png"))); // NOI18N
         jButton4.setText("CONTACTOS");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
         getContentPane().add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 400, 420, -1));
 
         fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ImagenesMenuPrincipal/fondo.jpg"))); // NOI18N
@@ -67,6 +87,34 @@ public class frmMenuPrincipal extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        frmDestino form = new frmDestino();
+        form.setVisible(true);
+        this.hide();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        frmCompraBoletos form = new frmCompraBoletos();
+        form.setVisible(true);
+        this.hide();
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        frmReservas form = new frmReservas();
+        form.setVisible(true);
+        this.hide();
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+        frmContactos form = new frmContactos();
+        form.setVisible(true);
+        this.hide();
+    }//GEN-LAST:event_jButton4ActionPerformed
 
     /**
      * @param args the command line arguments
